85554aa02d7f2e8209e032bac55664689680d08f
manuelcanseco <manucan999@gmail.com>
2025-11-28 16:07:48 -0300

    Add files via upload

---
+++ p5/funciones.js
@@ -0,0 +1,43 @@
+let nombresImagenes = [
+  "data/escena0.webp", "data/escena1.webp", "data/escena2.webp", "data/escena3.webp", "data/escena4.webp",
+  "data/escena5.webp", "data/escena6.webp", "data/escena7.webp", "data/escena8.webp", "data/escena9.webp",
+  "data/escena10.webp", "data/escena11.webp", "data/escena12.webp", "data/escena13.webp", "data/escena14.webp",
+  "data/escena15.webp", "data/escena16.webp"
+];
+
+let escenas = [
+    
+    ["Comenzar", 1, "", -1], 
+    
+    ["Escuchar escondido", 2, "Ignorar", 3], 
+    
+    ["Avanzar", 4, "", -1], 
+    
+    ["Avanzar", 6, "", -1], 
+    
+    ["Seguir el camino (Piedras)", 5, "Buscar refugio (Perdido)", 6], 
+    
+    ["Continuar", 13, "", -1], 
+    
+    ["Continuar", 7, "", -1], 
+    
+    ["Entrar", 8, "", -1], 
+    
+    ["Engañar al ogro", 10, "Escapar", 11], 
+    
+    ["Regresar a la casa", 7, "", -1], 
+    
+    ["Continuar", 12, "", -1], 
+    
+    ["Continuar", 12, "", -1], 
+    
+    ["Volver a la Guarida", 14, "", -1], 
+    
+    ["Reiniciar", -1, "", -1], 
+    
+    ["Negociar", 15, "Atacar", 16], 
+    
+    ["Reiniciar", -1, "", -1], 
+    
+    ["Reiniciar", -1, "", -1] 
+];
